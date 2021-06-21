efa73f5b3c43f300ef6d899b93d4e719c4824c26
Khaliddxx <doodz@aucegypt.edu>
2021-06-21 16:39:50 +0200

    Missing styling.

--- src/App.js
+++ src/App.js
@@ -7,89 +7,80 @@ import { Form, Modal, Button, Container } from "react-bootstrap";
 import { movies } from "./movies";
 
 function App() {
-    const [show, setShow] = useState(false);
-    const [data, setData] = useState(movies);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState(movies);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    return (
-        <div className="App">
-            <MovieList data={data} />
-            <Button
-                style={{ position: "relative", marginTop: "500px" }}
-                onClick={handleShow}
-            >
-                Add Movie
-            </Button>
-            <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Add a New Movie</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Enter the details of the movie you want to add.
-                </Modal.Body>
+  return (
+    <div className="App">
+      <MovieList data={data} />
+      <Button
+        style={{ position: "relative", marginTop: "500px" }}
+        onClick={handleShow}
+      >
+        Add Movie
+      </Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add a New Movie</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Enter the details of the movie you want to add.</Modal.Body>
 
-                <Container>
-                    <Form>
-                        <Form.Group controlId="movie-name">
-                            <Form.Label>Movie Name</Form.Label>
-                            <Form.Control placeholder="Enter Movie Name" />
-                        </Form.Group>
+        <Container>
+          <Form>
+            <Form.Group controlId="movie-name">
+              <Form.Label>Movie Name</Form.Label>
+              <Form.Control placeholder="Enter Movie Name" />
+            </Form.Group>
 
-                        <Form.Group controlId="movie-details">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control placeholder="Movie's Description" />
-                        </Form.Group>
+            <Form.Group controlId="movie-details">
+              <Form.Label>Description</Form.Label>
+              <Form.Control placeholder="Movie's Description" />
+            </Form.Group>
 
-                        <Form.Group controlId="poster-url">
-                            <Form.Label>Poster URL</Form.Label>
-                            <Form.Control placeholder="URL of Movie Poster" />
-                        </Form.Group>
+            <Form.Group controlId="poster-url">
+              <Form.Label>Poster URL</Form.Label>
+              <Form.Control placeholder="URL of Movie Poster" />
+            </Form.Group>
 
-                        <Form.Group>
-                            <Form.File
-                                id="upload-img"
-                                label="Upload movie poster"
-                            />
-                        </Form.Group>
+            <Form.Group controlId="movie-rating">
+              <Form.Label>Rating</Form.Label>
+              <Form.Control placeholder="Movie's Rating (out of 10)" />
+            </Form.Group>
+          </Form>
+        </Container>
 
-                        <Form.Group controlId="movie-rating">
-                            <Form.Label>Rating</Form.Label>
-                            <Form.Control placeholder="Movie's Rating (out of 10)" />
-                        </Form.Group>
-                    </Form>
-                </Container>
-
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Cancel
-                    </Button>
-                    <Button
-                        className="add-movie-btn"
-                        variant="primary"
-                        onClick={() => {
-                            setData((prev) => {
-                                prev.push({
-                                    title: "ahmed",
-                                    description: "ahmed",
-                                    posterURL: "ahmed",
-                                    rating: 10,
-                                });
-                                return [...prev];
-                            });
-                        }}
-                    >
-                        Add Movie
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            className="add-movie-btn"
+            variant="primary"
+            onClick={() => {
+              setData((prev) => {
+                prev.push({
+                  title: "ahmed",
+                  description: "ahmed",
+                  posterURL: "ahmed",
+                  rating: 10,
+                });
+                return [...prev];
+              });
+            }}
+          >
+            Add Movie
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,26 +1,26 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const MovieList = ({ data }) => {
-    return (
-        <div>
-            <Container>
-                <Row>
-                    {data.map((movie) => (
-                        <Col>
-                            <MovieCard
-                                t={movie.title}
-                                d={movie.description}
-                                url={movie.posterURL}
-                                rate={movie.rating}
-                            />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div>
+      <Container>
+        <Row>
+          {data.map((movie) => (
+            <Col>
+              <MovieCard
+                t={movie.title}
+                d={movie.description}
+                url={movie.posterURL}
+                rate={movie.rating}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default MovieList;

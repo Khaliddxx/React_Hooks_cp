469611d6c31cea2a21714bbbc3b244b6b344d076
Khaliddxx <doodz@aucegypt.edu>
2021-06-24 17:26:25 +0200

    Adding Movie errs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { useRef } from "react";
 import MovieCard from "./Components/MovieCard";
 import MovieList from "./Components/MovieList";
-
 import { movies } from "./movies";
 import AddMovieModal from "./Components/AddMovieModal";
 

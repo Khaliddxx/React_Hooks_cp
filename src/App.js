cde7d46b08d1071f8cf2ea08663bb924b5fd8ce2
Khaliddxx <doodz@aucegypt.edu>
2021-06-27 23:30:02 +0200

    added rate filtering, minor duplication bug

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import "./App.css";
-import { useState } from "react";
-import { useRef } from "react";
-import MovieCard from "./Components/MovieCard";
+import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import MovieList from "./Components/MovieList";
 import { movies } from "./movies";
 import AddMovieModal from "./Components/AddMovieModal";
 
 function App() {
-    const [data, setData] = useState(movies);
+  const [data, setData] = useState(movies);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [range, setRange] = useState(0);
 
-    return (
-        <div className="App">
-            <MovieList data={data} />
-            <AddMovieModal onAdd={setData} />
-        </div>
-    );
+  useEffect(() => {
+    clearFilters();
+    // eslint-disable-next-line
+  }, [movies]);
+
+  const clearFilters = () => {
+    setRange(0);
+    setFilteredMovies([...movies]);
+  };
+
+  const filter = () => {
+    let newMovies = movies;
+    newMovies = newMovies.filter((m) => m.rating <= range);
+    setFilteredMovies([...newMovies]);
+  };
+
+  return (
+    <div className="App">
+      <AddMovieModal onAdd={setData} />
+      <input
+        type="range"
+        value={range * 10}
+        class="slider"
+        id="rate"
+        onChange={(e) => setRange(Math.round(Number(e.target.value) / 10))}
+      ></input>
+      <Button className="mr-3" onClick={filter}>
+        Apply Filter
+      </Button>
+      <span className="m-auto">{range}</span>
+      <MovieList data={filteredMovies} />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,58 +1,38 @@
 import React from "react";
 import { useState } from "react";
-import "./MovieCard.css";
 import { card } from "react-bootstrap";
 
 const MovieCard = (props) => {
-    const [data, setData] = useState({
-        title: props.t,
-        description: props.d,
-        posterURL: props.url,
-        rating: props.rate,
-    });
+  const [data, setData] = useState({
+    title: props.t,
+    description: props.d,
+    posterURL: props.url,
+    rating: props.rate,
+  });
 
-    // const handleClick = () =>
-    //   setData({
-    //     title: "",
-    //     description: "",
-    //     posterURL: "",
-    //     rating: "",
-    //   });
+  return (
+    <>
+      <div
+        class="card"
+        style={{
+          width: "14rem",
+          height: "34rem",
 
-    return (
-        <>
-            {/* <div className="movie-card">
-        <div className="justify-content-md-center">
-          <h5>{data.title}</h5>
-          <h6>{data.description}</h6>
-          <img className="movie-img" src={data.posterURL} alt="movie..." />
-          <h2>{data.rating}</h2>
+          boxShadow: "10px 30px 10px #D6CED2",
+          borderRadius: "10px",
+          marginBottom: "5px",
+          overflow: "hidden",
+        }}
+      >
+        <img class="card-img-top" src={data.posterURL} alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">{data.title}</h5>
+          <p class="card-text">{data.description}</p>
         </div>
-      </div> */}
-
-            <div
-                class="card"
-                style={{
-                    width: "15rem",
-                    height: "33rem",
-                    border: "2px solid",
-                    borderRadius: "10px",
-                    marginBottom: "5px",
-                    overflow: "hidden",
-                }}
-            >
-                <img
-                    class="card-img-top"
-                    src={data.posterURL}
-                    alt="Card image cap"
-                />
-                <div class="card-body">
-                    <h5 class="card-title">{data.title}</h5>
-                    <p class="card-text">{data.description}</p>
-                </div>
-            </div>
-        </>
-    );
+        <span className="card-footer">{data.rating}</span>
+      </div>
+    </>
+  );
 };
 
 export default MovieCard;
